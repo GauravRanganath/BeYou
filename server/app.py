01e9d520d76789e49a58b5b060f36f4f3c3ee353
Gaurav Ranganath <gauravranganath@Gauravs-MacBook-Pro.local>
2023-02-18 18:13:26 -0500

    add data

--- server/app.py
+++ server/app.py
@@ -1,5 +1,8 @@
 from data_processing import speechToText, videoToAudio
-from flask import Flask
+import os
+from flask import Flask, request, session
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
 
@@ -10,9 +13,22 @@ DIR = "./data/"
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
 @app.route("/test")
 def test():
     videoToAudio(DIR+"videoTest.mp4")
     text = speechToText(DIR+"videoTest.mp4_audio.wav")
     return text
+
+@app.route('/upload', methods=['POST'])
+def fileUpload():
+    target=os.path.join("./data",'test_docs')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    file = request.files['file'] 
+    filename = file.filename
+    destination="/".join([target, filename])
+    file.save(destination)
+    response="Whatever you wish too return"
+    return response
+
+CORS(app, expose_headers='Authorization')
