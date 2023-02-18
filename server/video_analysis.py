f986db23000dd3267aa3edfbee5c2a319f982e01
Arsh Kadakia <arsh.kadakia18@gmail.com>
2023-02-18 15:42:01 -0500

    added analysis files

---
+++ server/video_analysis.py
@@ -0,0 +1,15 @@
+from deepface import DeepFace
+
+class VideoAnalysis(object):
+    def __init__(self):
+        self.actions = ("emotion")
+
+    def analyze(self, img_path):
+        objs = DeepFace.analyze(img_path=img_path, actions=self.actions)
+        for emotion in objs[0]['emotion']:
+            objs[0]['emotion'][emotion] = objs[0]['emotion'][emotion]/100
+        return objs
+
+if __name__=="__main__":
+    video_analyzer = VideoAnalysis()
+    print(video_analyzer.analyze("happy_face.png"))
