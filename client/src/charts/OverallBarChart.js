24293a16b52a16e54810711fb82e08dd343d7894
Gaurav Ranganath <gauravranganath@Gauravs-MacBook-Pro.local>
2023-02-19 01:41:06 -0500

    add dashboard frontend

---
+++ client/src/charts/OverallBarChart.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+      display: "true",
+    },
+    title: {
+      display: false,
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [1, 2, 5, 2, 8, 0, 1],
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
+};
+
+export function OverallBarChart() {
+  return (
+    <Bar
+      options={{
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "top",
+            display: false,
+          },
+          title: {
+            display: false,
+          },
+        },
+      }}
+      data={data}
+    />
+  );
+}
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,25 @@
+import OverallMaster from "../charts/OverallMaster";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+
 const Dashboard = () => {
-    return <h1>Dashboard</h1>;
-  };
-  
-  export default Dashboard;
-  
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Dashboard</h1>
+          </Col>
+        </Row>
+        <Row>
+            <Col>
+                <OverallMaster></OverallMaster>
+            </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
