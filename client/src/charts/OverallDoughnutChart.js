7e818a2414d0e9f27d9b539350a8fb13595c4c3f
Gaurav Ranganath <gauravranganath@Gauravs-MacBook-Pro.local>
2023-02-19 02:04:45 -0500

    add dashboard

--- client/src/charts/OverallDoughnutChart.js
+++ client/src/charts/OverallDoughnutChart.js
@@ -5,11 +5,11 @@ import { Doughnut } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['Anger', 'Disgust', 'Fear', 'Joy', 'Neutral', 'Sadness', 'Surprise'],
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 19, 3, 5, 2, 3, 3],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
